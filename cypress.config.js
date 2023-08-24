4c40038548d8aa59e79551b8e14fd0e03e3c41e4
mzuhairiy <atasnamazuhair@gmail.com>
2023-08-24 13:48:05 +0700

    add config

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,13 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: false,
-    experimentalRunAllSpecs: true,
+    experimentalRunAllSpecs: true, //tambahkan jika ingin run all specs di cypress open
     baseUrl:"https://kasirdemo.belajarqa.com/",
     pageLoadTimeout:120000,
     setupNodeEvents(on, config) {
       config.specPattern = [
-        'cypress/e2e/login.cy.js',
-        'cypress/e2e/category.cy.js',
+        'cypress/e2e/1login.cy.js',
+        'cypress/e2e/2category.cy.js',
       ]
       return config
     },
