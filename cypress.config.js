e7003cc0258a6daff53bee412fe21d3f7f8f3e50
Zuhair <zuhairimuhammad13@gmail.com>
2023-12-14 20:21:25 +0700

    update locator

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'yd2xs1',
   e2e: {
     chromeWebSecurity: false,
     experimentalRunAllSpecs: true, //tambahkan jika ingin run all specs di cypress open
